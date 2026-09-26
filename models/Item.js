54adc570141f1cb7b249fbd9b206787e0dce4420
NethmiDhananjana1011 <nethnethmidhananjana1011@gmail.com>
2026-09-26 15:07:30 +0530

    chage Item Schema(Add userId)

--- models/Item.js
+++ models/Item.js
@@ -1,9 +1,10 @@
 const mongoose = require('mongoose');
 
 const itemSchema = new mongoose.Schema({
+  userId: {type:mongoose.Types.ObjectId,ref: 'User',require:true},
   name: { type: String, required: true },
   material: { type: String, required: true },
-  condition: { type: String, required: true }, // මෙතන Number වෙනුවට String වෙන්න ඕන
+  condition: { type: String, required: true }, 
   description: { type: String },
 }, { timestamps: true });
 
